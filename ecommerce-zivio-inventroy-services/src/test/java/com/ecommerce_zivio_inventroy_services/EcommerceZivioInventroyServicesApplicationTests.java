37cef1da2e6e59b50541ee61d55401ad48ea6536
Sujitclear <adsulsujit2006@gmail.com>
2026-09-07 15:22:53 +0530

    Complete folder strcture with required dependency

---
+++ ecommerce-zivio-inventroy-services/src/test/java/com/ecommerce_zivio_inventroy_services/EcommerceZivioInventroyServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce_zivio_inventroy_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceZivioInventroyServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
