69c0ab279d32c8f4869a14a4f118f31e4cf64193
Sujitclear <adsulsujit2006@gmail.com>
2026-09-07 15:38:44 +0530

    Complete folder strcture for order service and required dependency

---
+++ ecommerce-zivio-order-services/src/main/java/com/ecommerce_zivio_order_services/EcommerceZivioOrderServicesApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce_zivio_order_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceZivioOrderServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceZivioOrderServicesApplication.class, args);
+	}
+
+}
