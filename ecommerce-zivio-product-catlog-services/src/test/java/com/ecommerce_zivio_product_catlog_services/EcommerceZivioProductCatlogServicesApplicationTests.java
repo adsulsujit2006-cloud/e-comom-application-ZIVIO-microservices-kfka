844839b4982b817ef0ae9f781a85be89c8b9591c
Sujitclear <adsulsujit2006@gmail.com>
2026-09-07 15:17:11 +0530

    Complete product catlog services with required dependency

---
+++ ecommerce-zivio-product-catlog-services/src/test/java/com/ecommerce_zivio_product_catlog_services/EcommerceZivioProductCatlogServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce_zivio_product_catlog_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceZivioProductCatlogServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
