3d48b6f7677bfbc4c487e9d425294d3af2df81d5
Sujitclear <adsulsujit2006@gmail.com>
2026-09-13 06:25:43 +0530

    Configuration like DB erukaClient and hibernet

--- ecommerce-zivio-user-services/src/main/java/com/ecommerce_zivio_user_services/EcommerceZivioUserServicesApplication.java
+++ ecommerce-zivio-user-services/src/main/java/com/ecommerce_zivio_user_services/EcommerceZivioUserServicesApplication.java
@@ -2,8 +2,10 @@ package com.ecommerce_zivio_user_services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EcommerceZivioUserServicesApplication {
 
 	public static void main(String[] args) {
